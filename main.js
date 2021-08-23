43e1491c04782a8c1b8ec6351a119bead526e6ab
Ezra <beneaththecurve@gmail.com>
2021-08-23 17:51:08 -0600

    Update main.js
    counters test

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+var yesCount = 0;
+var noCount = 0;
+
 function revealRules() {
 	if (document.getElementById("ruleTxt").innerHTML === "") {
 		document.getElementById("ruleTxt").innerHTML = "Mucho Texto";
 	} else {
 		document.getElementById("ruleTxt").innerHTML = "";
 	}
+}
+
+function yes() {
+	yesCount++;
+	document.getElementById("yesCount").innerHTML = yesCount;
+}
+
+function no() {
+	noCount++;
+	document.getElementById("noCount").innerHTML = noCount;
 }
