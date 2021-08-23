492026c395ed6eb0ce5d94696c5ae0df4055487e
Ezra <beneaththecurve@gmail.com>
2021-08-23 00:54:55 -0600

    Update main.js
    semicolon aaaaaaaa

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function revealRules() {
 	if (document.getElementById("ruleTxt").innerHTML === "" {
 		document.getElementById("ruleTxt").innerHTML = "Mucho Texto";
-		console.log(document.getElementById("ruleTxt").innerHTML)
+		console.log(document.getElementById("ruleTxt").innerHTML);
 	} else {
 		document.getElementById("ruleTxt").innerHTML = "";
 		console.log("Funciono");
