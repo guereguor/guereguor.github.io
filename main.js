3dd28b089c392baf6aa24963a67de6b1f2aa3803
Ezra <beneaththecurve@gmail.com>
2021-08-23 01:19:00 -0600

    Made rule button
    Please work

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 function revealRules() {
 	if (document.getElementById("ruleTxt").innerHTML === "") {
 		document.getElementById("ruleTxt").innerHTML = "Mucho Texto";
-	} 
+	} else {
+		document.getElementById("ruleTxt").innerHTML = "";
+	}
 }
