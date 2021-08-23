c21dfab509745848a5bf46d40f9620ad879b9b00
Ezra <beneaththecurve@gmail.com>
2021-08-23 01:07:07 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,5 @@ function revealRules() {
 	if (document.getElementById("ruleTxt").innerHTML === "") {
 		document.getElementById("ruleTxt").innerHTML = "Mucho Texto";
 		console.log(document.getElementById("ruleTxt").innerHTML);
-	} else {
-		document.getElementById("ruleTxt").innerHTML = "";
-		console.log("Funciono");
-	}
+	} 
 }
